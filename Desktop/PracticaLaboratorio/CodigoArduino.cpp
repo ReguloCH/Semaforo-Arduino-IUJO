58867ee87a815c09d8fa53e1595e8a731da5412b
Franyerson02 <franyersongonzalez2@gmail.com>
2026-03-06 08:08:30 -0400

    agregando logica circuito

--- Desktop/PracticaLaboratorio/CodigoArduino.cpp
+++ Desktop/PracticaLaboratorio/CodigoArduino.cpp
@@ -21,6 +21,61 @@ void setup() {
     pinMode(pin, OUTPUT);
   }
 }
+
+void loop() {
+
+ 
+  setRojoTodos();
+  verde(V1, R1);
+  delay(5000);
+
+  amarillo(V1, AM1);
+  delay(2000);
+
+  rojo(R1);
+  pasoPeatonal();
+
+ 
+  setRojoTodos();
+  verde(V2, R2);
+  delay(5000);
+
+  amarillo(V2, AM2);
+  delay(2000);
+
+  rojo(R2);
+  pasoPeatonal();
+
+ 
+  setRojoTodos();
+  verde(V3, R3);
+  delay(5000);
+
+  amarillo(V3, AM3);
+  delay(2000);
+
+  rojo(R3);
+  pasoPeatonal();
+}
+
+
+void setRojoTodos() {
+  digitalWrite(R1, HIGH);
+  digitalWrite(R2, HIGH);
+  digitalWrite(R3, HIGH);
+
+  digitalWrite(V1, LOW);
+  digitalWrite(V2, LOW);
+  digitalWrite(V3, LOW);
+
+  digitalWrite(AM1, LOW);
+  digitalWrite(AM2, LOW);
+  digitalWrite(AM3, LOW);
+
+  apagarPeatonal();
+}
+
+
 void verde(int verdePin, int rojoPin) {
   digitalWrite(rojoPin, LOW);
   digitalWrite(verdePin, HIGH);
